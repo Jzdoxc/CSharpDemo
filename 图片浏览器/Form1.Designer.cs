97c183d0099f50b518c03b66761e506411f52159
Jzdoxc <jzdoxc@gmail.com>
2017-09-23 08:56:18 +0800

    缩放已解决，位置未解决

--- 图片浏览器/Form1.Designer.cs
+++ 图片浏览器/Form1.Designer.cs
@@ -31,10 +31,12 @@
             this.components = new System.ComponentModel.Container();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button2 = new System.Windows.Forms.Button();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
@@ -51,26 +53,17 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBox1.Location = new System.Drawing.Point(539, 53);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(458, 433);
+            this.pictureBox1.Size = new System.Drawing.Size(507, 423);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(339, 187);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "开始";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
             // 
             // timer1
             // 
-            this.timer1.Enabled = true;
             this.timer1.Interval = 500;
             this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
             // 
@@ -78,7 +71,7 @@
             // 
             this.button2.Location = new System.Drawing.Point(339, 253);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(98, 23);
             this.button2.TabIndex = 3;
             this.button2.Text = "暂停/继续";
             this.button2.UseVisualStyleBackColor = true;
@@ -92,17 +85,48 @@
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(179, 56);
             this.trackBar1.TabIndex = 4;
-            this.trackBar1.Value = 20;
+            this.trackBar1.Value = 5;
             this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(447, 391);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "保存";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(339, 201);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(98, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "开始预览";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(319, 311);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "速度调节";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1087, 610);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
@@ -119,10 +143,12 @@
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- 图片浏览器/Form1.cs
+++ 图片浏览器/Form1.cs
@@ -16,19 +16,39 @@ namespace 图片浏览器
         public Form1()
         {
             InitializeComponent();
+            this.pictureBox1.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseWheel);
+
         }
+
+        private void pictureBox1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta >= 0)
+            {
+                pictureBox1.Width = (int)(pictureBox1.Width * 1.1);//因为Widthh和Height都是int类型，所以要强制转换一下-_-||  
+                pictureBox1.Height = (int)(pictureBox1.Height * 1.1);
+                pictureBox1.Location=new System.Drawing.Point(500, 0);
+            }
+            else
+            {
+                pictureBox1.Width = (int)(pictureBox1.Width * 0.9);
+                pictureBox1.Height = (int)(pictureBox1.Height * 0.9);
+                pictureBox1.Location = new Point(300, 0);
+            }
+        }
+
         List<string> list = new List<string>();
         int x = 0;
+        Random r = new Random();
         private void Form1_Load(object sender, EventArgs e)
         {
-            //string[] path = Directory.GetFiles(@"C:\Users\anpenghe\Documents\Tencent Files\1487759857\Image\Group");
-            string[] newpath = Directory.GetFiles(@"C:\Users\anpenghe\Pictures\Saved Pictures");
-            for (int i = 0; i < newpath.Length; i++)
+            string[] path = Directory.GetFiles(@"C:\Users\anpenghe\Documents\Tencent Files\1487759857\Image\Group");
+            string[] path2 = Directory.GetFiles(@"C:\Users\anpenghe\Pictures\Camera Roll");
+            for (int i = 0; i < path.Length; i++)
             {
-                list.Add(newpath[i]);
-                //File.Move(path[i], @"C:\Users\anpenghe\Pictures\Saved Pictures\"+i+".jpg");
-                listBox1.Items.Add(Path.GetFileName(newpath[i]));
+                list.Add(path[i]);
+                listBox1.Items.Add(Path.GetFileName(path[i]));
             }
+
         }
 
         private void listBox1_DoubleClick(object sender, EventArgs e)
@@ -37,16 +57,14 @@ namespace 图片浏览器
         }
 
 
-
-        private void Button1_Click(object sender, EventArgs e)
+        private void ShowPic()
         {
             listBox1.SetSelected(x, true);
             pictureBox1.ImageLocation = list[x++];
-
         }
         private void Timer1_Tick(object sender, EventArgs e)
         {
-            button1.PerformClick();
+            ShowPic();
         }
         bool flag = true;
         private void button2_Click(object sender, EventArgs e)
@@ -54,7 +72,7 @@ namespace 图片浏览器
             if (flag)
             {
 
-            timer1.Stop();
+                timer1.Stop();
                 flag = false;
             }
             else
@@ -66,7 +84,24 @@ namespace 图片浏览器
 
         private void trackBar1_ValueChanged(object sender, EventArgs e)
         {
-            timer1.Interval = trackBar1.Value*100;
+            timer1.Interval = trackBar1.Value * 100;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            File.Move(pictureBox1.ImageLocation, @"C:\Users\anpenghe\Pictures\Camera Roll\" +DateTime.Now.ToString("yyyy-MM-dd HHmmss") + ".jpg");
+            x++;
+            ShowPic();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBox1.Focus();
         }
     }
 }
